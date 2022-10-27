cbdee712c786bfede297b08aa34f09039ab21908
Yong Da Li <yongdali314@gmail.com>
2022-10-27 00:01:17 -0400

    formated files with black

--- sample_data/quick_plot.py
+++ sample_data/quick_plot.py
@@ -1,21 +1,20 @@
-'''
+"""
 Yong Da Li
 Monday, October 17, 2022
 
 quick and dirty script to plot the peak data
-'''
+"""
 
 from matplotlib import pyplot as plt
 
 data = []
 
-with open('peak_typical.txt', 'r') as file:
+with open("peak_typical.txt", "r") as file:
     for row in file:
-        a = row.strip("\n").strip(' ').split(' ')
+        a = row.strip("\n").strip(" ").split(" ")
         for elem in a:
-            if elem != '':
+            if elem != "":
                 data.append(int(elem))
 
 plt.plot(data)
 plt.show()
-        
