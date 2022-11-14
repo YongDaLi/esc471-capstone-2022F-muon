33177d692f8a779cd2e8024c3b1630ad68a54565
Soroush Khoubyarian <40778228+soroush1379@users.noreply.github.com>
2022-11-14 01:01:54 -0500

    Updating pictures

--- waveform_fetch/event_analyzer.py
+++ waveform_fetch/event_analyzer.py
@@ -5,9 +5,10 @@ import shutil
 
 eventsDirectory = "./events"
 peaksDirectory = "./peaks"
-peakMin = [-3, -3, -3, -3] # Different for different channels
-peakMax = [-0.10, -0.03, -0.1, -0.1] # Different for different channels
-peakThreshold = [1e-6, 1e-6, 1e-6, 1e-6] # Different for different channels
+peakMin = [-3, -3, -3, -3]  # Different for different channels
+peakMax = [-0.10, -0.03, -0.1, -0.1]  # Different for different channels
+peakThreshold = [1e-6, 1e-6, 1e-6, 1e-6]  # Different for different channels
+
 
 def readFile(address):
     file = open(eventsDirectory + "/" + address, "r")
@@ -19,11 +20,12 @@ def readFile(address):
         s = line.split(',')
         t += [float(s[0])]
         for j in range(4):
-            V[j] += [float(s[j+1])]
+            V[j] += [float(s[j + 1])]
 
     return [t, V]
 
-def findPeaks(channel, t, V): # Returns the avg index, not time
+
+def findPeaks(channel, t, V):  # Returns the avg index, not time
     """
     Returns the indices of the peaks
     """
@@ -45,6 +47,7 @@ def findPeaks(channel, t, V): # Returns the avg index, not time
 
     return peaks
 
+
 if not os.path.exists(eventsDirectory):
     print("No event directory found")
     exit()
@@ -55,16 +58,16 @@ if os.path.exists(peaksDirectory):
 os.makedirs(peaksDirectory)
 
 eventFiles = [s for s in os.listdir(eventsDirectory) if s.startswith("event") and s.endswith(".txt")]
-eventFiles = sorted(eventFiles, key=lambda s : int(s.split("t")[1].split(".")[0]))
+eventFiles = sorted(eventFiles, key=lambda s: int(s.split("t")[1].split(".")[0]))
 
 info = []
 for (i, address) in enumerate(eventFiles):
-    print("Event " + str(i+1) + "/" + str(len(eventFiles)))
+    print("Event " + str(i + 1) + "/" + str(len(eventFiles)))
 
     [t, V] = readFile(address)
     info += [[]]
 
-    fig, ax = plt.subplots(2, 2, figsize=(12,7))
+    fig, ax = plt.subplots(2, 2, figsize=(12, 7))
 
     for j in range(4):
         n = j // 2
@@ -72,16 +75,16 @@ for (i, address) in enumerate(eventFiles):
 
         peaks = findPeaks(j, t, V[j])
 
-        ax[n, m].set_title("Channel " + str(j+1) + "; " + str(len(peaks)) + " Peaks")
-        ax[n, m].plot(t, V[j], c=(0,0,0), linewidth=0.5)
+        ax[n, m].set_title("Channel " + str(j + 1) + "; " + str(len(peaks)) + " Peaks")
+        ax[n, m].plot(t, V[j], c=(0, 0, 0), linewidth=0.5)
         for p in peaks:
-            ax[n, m].axvline(t[p], c=(1,0,0), linewidth=1.5)
-            info[-1] += [(j+1, t[p])]
+            ax[n, m].axvline(t[p], c=(1, 0, 0), linewidth=1.5)
+            info[-1] += [(j + 1, t[p])]
 
     plt.savefig(peaksDirectory + "/" + address.split('.')[0] + ".jpg")
     plt.close(fig)
 
-sinfo = [sorted(d, key=lambda r : r[1]) for d in info]
+sinfo = [sorted(d, key=lambda r: r[1]) for d in info]
 lines = [",".join([str(l[0]) + "," + str(l[1]) for l in d]) for d in sinfo]
 
 outputFile = open("./output.txt", "w")
